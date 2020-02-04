7c60be30b8ab127a79f161c8ea59bc2d3fd7fd7e
pbinse.mcbi <pbinse.mcbi@gmail.com>
2020-02-04 08:14:40 +0800

    Add project files.

---
+++ EightMilesAPI/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+#from config import HOST
+
+from flask_sqlalchemy import get_debug_queries
+
+# from version import *
+from routes.usersroute import usersapi
+
+app = Flask(__name__)
+api_version = '/api/v1.0'
+
+"""
+Blueprint is structuring the api's
+"""
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+app.register_blueprint(usersapi, url_prefix=api_version)
+
+
+if __name__ == '__main__':
+    #app.debug = True
+    #app.run(host=HOST['HOST'], port=HOST['PORT'])
+
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
